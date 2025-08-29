33f4515420b9c60f474624d9416239ee759b0ad4
LDS <luanddt2001@gmail.com>
2025-08-29 22:18:23 +0700

    update auth layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,63 @@
+import type { Metadata } from "next";
+import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: "Auth - StoreIt",
+  description: "Cloud Storage Platform"
+};
+
+const AuthLayout = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="flex min-h-screen">
+      <section className="bg-primary p-10 xl:w-2/5 w-1/2 lg:flex-center hidden">
+        <div className="max-w-[430px] flex flex-col items-center gap-12">
+          <div className="w-full">
+            <Image
+              src="/assets/icons/logo-full-white.svg"
+              alt="StoreIt"
+              width={224}
+              height={82}
+              className="object-contain"
+            />
+          </div>
+
+          <div className="text-primary-foreground flex flex-col gap-5">
+            <h1 className="h1">
+              Manage your files the best way
+            </h1>
+
+            <p className="body-1">
+              Awesome, we&#39;ve created the perfect place for you to store all your documents.
+            </p>
+          </div>
+
+          <Image
+            src="/assets/images/files-auth.png"
+            alt="Files"
+            width={342}
+            height={342}
+            className="object-contain hover:rotate-2 hover:scale-105 transition-all"
+          />
+        </div>
+      </section>
+
+      <section className="bg-background p-10 max-lg:px-4 flex-center flex-1 flex-col max-lg:justify-start gap-16">
+        <Image
+          src="/assets/icons/logo-full-primary.svg"
+          alt="StoreIt"
+          width={224}
+          height={82}
+          className="object-contain lg:hidden"
+        />
+
+        {children}
+      </section>
+    </div>
+  );
+};
+
+export default AuthLayout;
