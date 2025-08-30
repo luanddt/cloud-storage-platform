77c14894464d6826b80ebe8f0c3c084b436d4067
LDS <luanddt2001@gmail.com>
2025-08-30 16:19:13 +0700

    update auth form

---
+++ components/AuthForm.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+type AuthFormProps = "sign-in" | "sign-up";
+
+const authFormSchema = (formType: AuthFormProps) => {
+  return (
+    z.object({
+      email: z.email(),
+      fullName: formType === "sign-up" ? z.string().min(2).max(50) : z.string().optional()
+    })
+  );
+};
+
+const AuthForm = ({
+  type
+}: {
+  type: AuthFormProps;
+}) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const formSchema = authFormSchema(type);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: ""
+    }
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full max-w-[580px] flex flex-col lg:gap-8 gap-6"
+        >
+          <h1 className="h1 max-lg:text-center">
+            {type === "sign-in" ? "Login" : "Create Account"}
+          </h1>
+
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="p-4 shadow-drop-1 rounded-[12px] flex flex-col gap-1.5">
+                    <FormLabel>Full Name</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        placeholder="Enter Your Full Name"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )}
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="p-4 shadow-drop-1 rounded-[12px] flex flex-col gap-1.5">
+                  <FormLabel>Email</FormLabel>
+
+                  <FormControl>
+                    <Input
+                      placeholder="Enter Your Email"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            type="submit"
+            disabled={isLoading}
+          >
+            {type === "sign-in" ? "Login" : "Create Account"}
+
+            {isLoading && (
+              <Image
+                src="/assets/icons/loading-white.svg"
+                alt="Loading"
+                width={24}
+                height={24}
+                className="object-contain animate-spin"
+              />
+            )}
+          </Button>
+
+          {errorMessage && (
+            <p className="bg-destructive/10 px-8 py-4 border border-destructive rounded-xl body-2 text-destructive text-center">
+              *{errorMessage}
+            </p>
+          )}
+
+          <div className="body-2 flex-center gap-1">
+            <p className="text-foreground">
+              {type === "sign-in" ? "Don't have an account?" : "Already have an account?"}
+            </p>
+
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="text-primary hover:text-primary/80 hover:underline"
+            >
+              {type === "sign-in" ? "Create Account" : "Login"}
+            </Link>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default AuthForm;
