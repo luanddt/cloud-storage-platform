8dd8562acc905d1be3967d776673415ba6260326
LDS <luanddt2001@gmail.com>
2025-09-06 20:55:51 +0700

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,33 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Search from "@/components/Search";
+import FileUploader from "@/components/FileUploader";
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="bg-background p-5 lg:py-7 sm:flex-between hidden">
+      <Search />
+
+      <div className="flex-center gap-4">
+        <FileUploader />
+
+        <form>
+          <Button
+            type="submit"
+            variant="logout"
+            className="p-3.5"
+          >
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="Logout"
+              width={24}
+              height={24}
+              className="object-contain"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
   );
 };
 
