1d7c88ebdbd1eee06a4e844e2b9f64733012178c
LDS <luanddt2001@gmail.com>
2025-09-11 23:30:13 +0700

    update create account

---
+++ lib/appwrite/config.ts
@@ -0,0 +1,11 @@
+export const appwriteConfig = {
+  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "",
+  projectName: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_NAME || "",
+  endpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "",
+  databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || "",
+  userCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USER_COLLECTION_ID || "",
+  fileCollectionId: process.env.NEXT_PUBLIC_APPWRITE_FILE_COLLECTION_ID || "",
+  bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID || "",
+  apiKey: process.env.APPWRITE_API_KEY || "",
+  devKey: process.env.APPWRITE_DEV_KEY || ""
+};
