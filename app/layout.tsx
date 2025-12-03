1a9e00b5807d4d0f179b58d1477f30939da5004c
LDS <luanddt2001@gmail.com>
2025-12-03 22:25:52 +0700

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "StoreIt",
+  description: "Cloud Storage Platform"
+};
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+});
+
+const RootLayout = ({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className}`}>
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
