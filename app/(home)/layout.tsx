31731b73ba1ca6d38074b9c84b70c82219a86d14
LDS <luanddt2001@gmail.com>
2025-12-05 20:35:23 +0700

    implement home layout

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,24 @@
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+
+const HomeLayout = ({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="h-screen flex">
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col">
+        <Header />
+
+        <main className="bg-[#F2F4F8] md:px-9 px-5 md:py-10 py-7 sm:mr-7 sm:mb-7 sm:rounded-[30px] flex-1 overflow-auto">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default HomeLayout;
