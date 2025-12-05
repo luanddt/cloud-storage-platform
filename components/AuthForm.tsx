6abf8b0910498fca927e6d2bed13dfd455c72bc4
LDS <luanddt2001@gmail.com>
2025-12-05 17:22:52 +0700

    implement otp modal

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,6 +17,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { createAccount } from "@/lib/actions/user.actions";
+import OTPModal from "@/components/OTPModal";
 
 const authFormSchema = (formType: FormType) => {
   return (
@@ -62,28 +63,51 @@ const AuthForm = ({
   };
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="w-full max-w-[580px] flex flex-col lg:gap-8 gap-6"
-      >
-        <h1 className="h1 max-lg:text-center">
-          {type === "sign-in" ? "Login" : "Create Account"}
-        </h1>
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full max-w-[580px] flex flex-col lg:gap-8 gap-6"
+        >
+          <h1 className="h1 max-lg:text-center">
+            {type === "sign-in" ? "Login" : "Create Account"}
+          </h1>
 
-        {type === "sign-up" && (
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="p-4 shadow-drop-1 rounded-[12px] flex flex-col gap-1.5">
+                    <FormLabel>Full Name</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        type="text"
+                        placeholder="Enter Your Full Name"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )}
+
           <FormField
             control={form.control}
-            name="fullName"
+            name="email"
             render={({ field }) => (
               <FormItem>
                 <div className="p-4 shadow-drop-1 rounded-[12px] flex flex-col gap-1.5">
-                  <FormLabel>Full Name</FormLabel>
-
+                  <FormLabel>Email</FormLabel>
                   <FormControl>
                     <Input
-                      type="text"
-                      placeholder="Enter Your Full Name"
+                      type="email"
+                      placeholder="Enter Your Email"
                       {...field}
                     />
                   </FormControl>
@@ -93,66 +117,53 @@ const AuthForm = ({
               </FormItem>
             )}
           />
-        )}
 
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <div className="p-4 shadow-drop-1 rounded-[12px] flex flex-col gap-1.5">
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input
-                    type="email"
-                    placeholder="Enter Your Email"
-                    {...field}
-                  />
-                </FormControl>
-              </div>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <Button
-          type="submit"
-          disabled={isLoading}
-        >
-          <p className="button">
-            {type === "sign-in" ? "Login" : "Create Account"}
-          </p>
-
-          {isLoading && (
-            <Image
-              src="/assets/icons/loader.svg"
-              alt="Loader"
-              width={24}
-              height={24}
-              className="animate-spin"
-            />
-          )}
-        </Button>
-
-        {errorMessage && (
-          <p className="body-2 text-destructive text-center">
-            *{errorMessage}
-          </p>
-        )}
-
-        <div className="body-2 flex-center gap-1">
-          <p>{type === "sign-in" ? "Don't have an account?" : "Already have an account?"}</p>
-
-          <Link
-            href={type === "sign-in" ? "/sign-up" : "/sign-in"}
-            className="text-primary hover:text-primary/90 hover:underline"
+          <Button
+            type="submit"
+            className="px-4 py-6"
+            disabled={isLoading}
           >
-            {type === "sign-in" ? "Create Account" : "Login"}
-          </Link>
-        </div>
-      </form>
-    </Form>
+            <p className="button">
+              {type === "sign-in" ? "Login" : "Create Account"}
+            </p>
+
+            {isLoading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                alt="Loader"
+                width={24}
+                height={24}
+                className="animate-spin"
+              />
+            )}
+          </Button>
+
+          {errorMessage && (
+            <p className="body-2 text-destructive text-center">
+              *{errorMessage}
+            </p>
+          )}
+
+          <div className="body-2 flex-center gap-1">
+            <p>{type === "sign-in" ? "Don't have an account?" : "Already have an account?"}</p>
+
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="text-primary hover:text-primary/90 hover:underline"
+            >
+              {type === "sign-in" ? "Create Account" : "Login"}
+            </Link>
+          </div>
+        </form>
+      </Form>
+
+      {accountId && (
+        <OTPModal
+          accountId={accountId}
+          email={form.getValues("email")}
+        />
+      )}
+    </>
   );
 };
 
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { appwriteConfig } from "@/lib/appwrite/config";
 import { ID, Query } from "node-appwrite";
 import { parseStringify } from "@/lib/utils";
 import { avatarPlaceholderUrl } from "@/constants";
+import { cookies } from "next/headers";
 
 const handleError = (message: string, error: unknown) => {
   console.log(message, error);
@@ -66,4 +67,29 @@ export const createAccount = async ({
   };
 
   return parseStringify({ accountId });
+};
+
+export const verifySecret = async ({
+  accountId,
+  password
+}: {
+  accountId: string;
+  password: string;
+}) => {
+  try {
+    const { account } = await createAdminClient();
+
+    const session = await account.createSession(accountId, password);
+
+    (await cookies()).set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true
+    });
+
+    return parseStringify({ sessionId: session.$id });
+  } catch (error) {
+    handleError("Failed to verify OTP", error);
+  };
 };
