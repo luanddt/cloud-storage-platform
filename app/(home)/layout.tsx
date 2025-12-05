cb0fb40a9035c18a2b34f65542406f1bcf0be9ca
LDS <luanddt2001@gmail.com>
2025-12-06 00:44:50 +0700

    implement sidebar

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,14 +1,20 @@
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const HomeLayout = ({
+const HomeLayout = async ({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return redirect("/sign-in");
+
   return (
     <div className="h-screen flex">
-      <Sidebar />
+      <Sidebar {...currentUser} />
 
       <div className="flex-1 flex flex-col">
         <Header />
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,95 @@
-const Sidebar = () => {
+"use client";
+
+import { navItems } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Sidebar = ({
+  fullName,
+  email,
+  avatar
+}: SidebarProps) => {
+  const pathname = usePathname();
+
   return (
-    <div>Sidebar</div>
+    <aside className="px-5 py-7 sm:flex flex-col hidden">
+      <Link href="/">
+        <Image
+          src="/assets/icons/logo-full.svg"
+          alt="StoreIt"
+          width={160}
+          height={52}
+          className="h-[52px] max-lg:hidden"
+        />
+
+        <Image
+          src="/assets/icons/logo.svg"
+          alt="StoreIt"
+          width={52}
+          height={52}
+          className="h-[52px] lg:hidden"
+        />
+      </Link>
+
+      <ul className="mt-7 flex-1 flex flex-col gap-5">
+        {navItems.map(({ name, icon, url }) => (
+          <li key={name}>
+            <Link
+              href={url}
+              className={cn(
+                "hover:bg-primary lg:px-[30px] p-3.5 shadow-drop-2 lg:rounded-full rounded-[12px] group flex items-center gap-4",
+                pathname === url && "bg-primary hover:bg-primary/90"
+              )}
+            >
+              <Image
+                src={icon}
+                alt={name}
+                width={24}
+                height={24}
+                className={cn(
+                  "invert group-hover:invert-0 opacity-25 group-hover:opacity-100",
+                  pathname === url && "invert-0 opacity-100"
+                )}
+              />
+
+              <h5 className={cn("h5 group-hover:text-primary-foreground max-lg:hidden", pathname === url && "text-primary-foreground")}>
+                {name}
+              </h5>
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className="relative h-40 lg:mt-5 max-lg:hidden">
+        <Image
+          src="/assets/images/files-bg.png"
+          alt="Files"
+          fill
+        />
+      </div>
+
+      <div className="mt-5 flex items-center gap-4">
+        <Image
+          src={avatar}
+          alt={fullName}
+          width={52}
+          height={52}
+          className="object-cover rounded-full"
+        />
+
+        <div className="w-50 line-clamp-1 flex flex-col gap-1 max-lg:hidden">
+          <h5 className="h5">
+            {fullName}
+          </h5>
+
+          <p className="body-2 text-muted-foreground">
+            {email}
+          </p>
+        </div>
+      </div>
+    </aside>
   );
 };
 
