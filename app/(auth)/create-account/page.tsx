1454bfca78efd2a4a149d681304332af82011bbc
luanddt <luanddt2001@gmail.com>
2026-07-22 20:13:27 +0700

    feat(auth): implement auth layout

---
+++ app/(auth)/create-account/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create Account - Storage",
+  description: "Cloud Storage Platform"
+};
+
+const CreateAccount = () => {
+  return (
+    <div>CreateAccount</div>
+  );
+};
+
+export default CreateAccount;
---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login - Storage",
+  description: "Cloud Storage Platform"
+};
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  );
+};
+
+export default Login;
